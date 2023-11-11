0e56b91e1266ecc217238ca4aa3e0e0fa9a5f358
RitikShah <ritik10.shah@gmail.com>
2023-11-11 12:39:23 -0500

    it's beet time evrybody

--- get_book.py
+++
@@ -1,8 +0,0 @@
-import pyperclip
-import json
-
-j = json.loads(open("pages.json",'r').read())
-p = "["
-for page in j:
-    p+=('\''+json.dumps(page)+'\',')
-pyperclip.copy("/give @p written_book{title:\"\",author:\"\",pages:"+p+"]}")
